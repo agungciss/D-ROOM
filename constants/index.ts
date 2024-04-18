ea9e4964a1b0c270cee68122a197dfae2c7d4ad6
agoooeng <145472348+agungciss@users.noreply.github.com>
2024-04-18 08:51:12 +0700

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'Upcoming',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/previous',
+    label: 'Previous',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/recordings',
+    label: 'Recordings',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/personal-room',
+    label: 'Personal Room',
+  },
+];
+
+export const avatarImages = [
+  '/images/girl1.jpg',
+  '/images/boy1.jpg',
+  '/images/boy2.jpg',
+  '/images/boy3.jpg',
+  '/images/girl2.jpg',
+];
